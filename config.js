71343aaf04b808c78b064aeecef447adebf723ae
adekpp <adekpp@gmail.com>
2024-06-13 00:41:44 +0200

    commit

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   accountName: process.env.ACCOUNT_NAME,
   password: process.env.ACCOUNT_PASSWORD,
+  database: process.env.DB,
   
 
   commands: [`!gram`, `!nie gram`, `!lista`],
